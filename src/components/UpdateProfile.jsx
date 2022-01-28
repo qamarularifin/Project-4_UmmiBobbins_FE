8dd0e196f1fb5c060af532271baa3bac581c7f93
qamarularifin <qamarulafro89@gmail.com>
2022-01-28 17:18:03 +0800

    added handlesubmit func in updateprofile

--- src/components/UpdateProfile.jsx
+++ src/components/UpdateProfile.jsx
@@ -21,6 +21,18 @@ const UpdateProfile = () => {
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
+
+    const handleSubmit = (e) =>{
+        e.preventDefault() // prevents from refreshing
+        // validation
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError("Passwords do not match")
+        }
+
+        
+    }
+
+
   return (
   
     <>
@@ -29,7 +41,7 @@ const UpdateProfile = () => {
             <h2 className="text-center mb-4">Update Profile</h2>
             
             {error && <Alert variant="danger">{error}</Alert> }
-            <Form onSubmit={""}>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group id="email">
                     <Form.Label>Email</Form.Label>
                     <Form.Control type="email" ref={emailRef} required
@@ -39,14 +51,14 @@ const UpdateProfile = () => {
                 <Form.Group id="password">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" ref={passwordRef}  
-                        placeholder="Leave black to keep the same"
+                        placeholder="Leave blank to keep the same"
                     />
                 </Form.Group>
 
                 <Form.Group id="password-confirm">
                     <Form.Label>Password Confirmation</Form.Label>
                     <Form.Control type="password" ref={passwordConfirmRef}  
-                        placeholder="Leave black to keep the same"
+                        placeholder="Leave blank to keep the same"
                     />
                 </Form.Group>
 
