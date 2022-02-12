eb5780ad8d2c139386ca9f9e3c22660495bf500f
mhdhakeym <mhdhakeym786@gmail.com>
2022-02-12 14:33:27 +0800

    created displayBooking component

---
+++ src/components/displayBooking.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const displayBooking = () => {
+    return (
+        <>
+            <div>
+
+            </div>
+        </>
+    )
+};
+
+export default displayBooking;
